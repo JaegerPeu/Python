002a2e6333f4ea9369b703f12f8947d96b77f556
JaegerPeu <116598390+JaegerPeu@users.noreply.github.com>
2024-10-11 15:06:27 -0300

    Add files via upload

---
+++ garmin.py
@@ -0,0 +1,37 @@
+
+
+
+import streamlit as st
+from garminconnect import Garmin
+import pandas as pd
+
+# Configuração do título da página e barra lateral
+st.title("Preparação Maratona Fefê")
+st.sidebar.title("Login Garmin")
+
+# Entrada para o e-mail e senha
+email = st.sidebar.text_input("Email", type="default")
+password = st.sidebar.text_input("Password", type="password")
+
+# Função para fazer login e pegar os dados
+if st.sidebar.button("Get Activities"):
+    if email and password:
+        try:
+            # Login na conta Garmin
+            client = Garmin(email, password)
+            client.login()
+
+            # Obter atividades
+            activities = client.get_activities(0, 100)
+
+            # Converter atividades para DataFrame pandas
+            df = pd.DataFrame(activities)
+
+            # Mostrar os dados no app
+            st.write("Suas Atividades:")
+            st.dataframe(df)
+
+        except Exception as e:
+            st.error(f"Erro {e}")
+    else:
+        st.warning("Colocar email e senha.")
